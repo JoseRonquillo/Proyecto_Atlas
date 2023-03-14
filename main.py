67176ebd32b3e361c09cc85be02ff35d00f4b449
José Ronquillo <114964339+JoseRonquillo@users.noreply.github.com>
2023-03-14 09:55:15 -0600

    agruegue las entradas

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+max = 0
+menor= 0
 
+num1 = int(input("Agruegue el primer numero"))
+num2 = int(input("Agruegue el segundo numero"))
+
+if num1 < num2:
+  max = num1
+  menor = num2
+else:
+  max = num2
+  menor = num1
